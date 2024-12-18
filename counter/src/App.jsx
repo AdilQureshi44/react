1c63f64301d70434df8d6b445078d9566f80a900
Adil Qureshi <adil.aq48@gmail.com>
2024-12-18 23:30:48 +0530

    fix the counter value

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,25 +2,28 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [counter,setCounter] = useState(15)
-  const addValue = ()=>{
+  let [counter, setCounter] = useState(15)
+  const addValue = () => {
     counter = counter + 1;
+    if (counter > 20) {
+      counter = 20;
+    }
     setCounter(counter);
   }
-  const remove = ()=>{
-    counter = counter -1;
-    if(counter === -1){
-     counter = 0;
+  const remove = () => {
+    counter = counter - 1;
+    if (counter === -1) {
+      counter = 0;
     }
-    setCounter(counter); 
+    setCounter(counter);
   }
   return (
     <>
-     <h1>Counter {counter}</h1>
-     <button onClick={addValue}>Add</button>
-     <br></br>
-     <br></br>
-     <button onClick={remove}>Remove</button>
+      <h1>Counter {counter}</h1>
+      <button onClick={addValue}>Add</button>
+      <br></br>
+      <br></br>
+      <button onClick={remove}>Remove</button>
     </>
   )
 }
